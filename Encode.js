36d43c56345f10f7e19a90daf0de3c958c81587f
Seikareikou <seikareikou@qq.com>
2015-02-02 15:31:10 +0800

    Update Encode.js

--- Encode.js
+++ Encode.js
@@ -1,9 +1,9 @@
 function encrypted_id (id) {  
   var byte1 = "3go8&$8*3*3h0k(2)2"
   var byte2 = id
-  var byte1_len = byte1.length
+  //var byte1_len = byte1.length
   for(var i = 0;i != byte2.length;i++){
-      byte2[i] = byte2[i]^byte1[i%byte1_len]}
+      byte2[i] = byte2[i]^byte1[i%18]}
   return byte2
 }  
 
