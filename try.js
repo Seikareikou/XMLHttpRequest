378a2ca76f1ddc23d7ccdd0ffe161d72ab31e3fd
Seikareikou <seikareikou@qq.com>
2015-02-03 12:56:04 +0800

    Update try.js

--- try.js
+++ try.js
@@ -24,3 +24,16 @@ function encrypted_id (id) {
       byte2[i] = byte2[i]^byte1[i%18]}
   return byte2
 }  
+
+
+function id (id) {
+  var byte1 = "3go8&$8*3*3h0k(2)2";
+  var l=byte1.length;
+  var byte2 = id;
+  for(var i=0;i<=byte2.length;i++){ 
+      byte2=byte2.replace(byte2.charAt(i),byte2.charAt(i)^byte1.charAt(i%l));
+   
+    //alert(byte2.charAt(i));
+  } 
+   return byte2;
+}
