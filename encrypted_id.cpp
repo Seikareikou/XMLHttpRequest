2b0f2a45d1409490ff6d7544237ec2fa8024a054
Seikareikou <seikareikou@qq.com>
2015-02-11 13:22:00 +0800

    Create encrypted_id.cpp

---
+++ encrypted_id.cpp
@@ -0,0 +1,13 @@
+#include<QByteArray
+
+void encrypted_id (id){
+    	QByteArray byte1 = "3go8&$8*3*3h0k(2)2";
+    	QByteArray byte2 = id;
+    	int byte1_len = byte1.length;
+    	//int byte2_len = byte2.length;
+    	for(int i = 0;i < byte2.length;i++){
+    		byte2[i] = byte[i] ^ byte2[i % byte1.len]
+    		}
+    		
+    	}
+    	
